100d6118ce19cffad05a89116a5b330b7f29a311
Andrew Oroh <andreworoh.ao@gmail.com>
2024-03-27 17:07:16 +0700

    MotorQ app initial commit
    create app template using swiftUI

---
+++ MotorQ/MotorQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotorQ
+//
+//  Created by Andrew Oroh on 27/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotorQ/MotorQ/MotorQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorQApp.swift
+//  MotorQ
+//
+//  Created by Andrew Oroh on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MotorQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
