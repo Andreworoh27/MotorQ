558d709ec1df04cea39b8332175d9c06de5364c8
Andrew Oroh <andreworoh.ao@gmail.com>
2024-04-03 16:55:57 +0700

    Update homepage component and create color library

--- MotorQ/MotorQ/View/SplashScreen.swift
+++ MotorQ/MotorQ/View/SplashScreen.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SplashScreen: View {
     var body: some View {
         ZStack {
-            Color.orange
+            Color.primaryOrange
                 .ignoresSafeArea() // ganti warna
             Image("logo")
                 .resizable()
