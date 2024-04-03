6679c5c03f63a347c7edc42598f8188a3dde04d4
danieliank <daxter1835@gmail.com>
2024-04-03 16:54:12 +0700

    add navbar and about view

---
+++ MotorQ/MotorQ/Components/AboutNavComponent.swift
@@ -0,0 +1,20 @@
+//
+//  AboutNavComponent.swift
+//  MotorQ
+//
+//  Created by Daniel Ian on 03/04/24.
+//
+
+import SwiftUI
+
+struct AboutNavComponent: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+#Preview {
+    AboutNavComponent()
+}
---
+++ MotorQ/MotorQ/View/About.swift
@@ -0,0 +1,18 @@
+//
+//  About.swift
+//  MotorQ
+//
+//  Created by Daniel Ian on 03/04/24.
+//
+
+import SwiftUI
+
+struct About: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    About()
+}
